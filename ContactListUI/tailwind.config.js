d3b9c00bfc24dde0cc31728a299da3a1ae0834c4
Eduardo Vizicato <eduardovizicato@gmail.com>
2024-12-16 15:33:29 -0300

    creating table

--- ContactListUI/tailwind.config.js
+++ ContactListUI/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ["./src/**/*.{html,ts}",'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
+
   theme: {
     extend: {},
   },
-  plugins: [],
+
+  plugins: [require('flowbite/plugin')],
 }
 
